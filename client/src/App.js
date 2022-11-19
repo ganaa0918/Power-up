4c0de8b6db0c73eed46b11b7bf8c98609b816815
ganaa0918 <82430690+ganaa0918@users.noreply.github.com>
2022-11-20 00:04:06 +0800

    emhetgew

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,16 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import './app.css'
-import img1 from './components/navbar/images/logo1.png'
+import HeaderImg from "./components/home";
+
+
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
     <div>
       <div className="header">
       <Navbar  />  
-        <div class="text-center text-white mt-5">
-           <img src={img1} height="300px"/>
-           <h1 class=" text-lg-white " style={{fontSize:"40px"}}>FITNESS CLUB</h1>
-        </div>
+      <HeaderImg />
       </div>
-        
     </div>
   );
 }
--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Images from './images/logo1.png'
+import Images from '../images/logo1.png'
 function Navbars() {
   return (
     
