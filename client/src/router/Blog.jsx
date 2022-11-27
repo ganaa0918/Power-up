dac16f138cce9649ef6d5e8353b6b802ea86ac63
ganaa0918 <82430690+ganaa0918@users.noreply.github.com>
2022-11-28 00:00:00 +0800

    blog hiij duuslaa

--- client/src/router/Blog.jsx
+++ client/src/router/Blog.jsx
@@ -4,21 +4,36 @@ import Navbars from '../components/navbar/Navbar'
 import Blog1 from '../image/blog1.png'
 import Blog2 from '../image/blog2.png'
 import Blog3 from '../image/blog3.png'
-
+import {Link} from 'react-router-dom'
 function Blog() {
   return (
     <div className='blog'>
         <Navbars />
         <div className='container' style={{paddingTop:"100px"}}>
-            <div class="grid grid-rows-3 grid-flow-col gap-4">
-                <div class="row-span-3 ...">
+            <div class="grid grid-rows-3 grid-flow-col gap-4 sm:grid-cols-1">
+                <div className="row-span-3">
+                    <div className='relative '>
                     <img src={Blog1} alt="blog1" />
+                    <div className='absolute bottom-20 left-20 text-white'>
+                        <Link to={'#'}  className="no-underline text-white">Яагаад Иогоор хичээллэх хэрэгтэй вэ</Link>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-span-2 ...">
-                <img src={Blog2} alt="blog1" />
+                <div className="col-span-2 ...">
+                    <div className='relative'>
+                    <img src={Blog2} alt="blog1" />
+                    <div className='absolute bottom-10 left-10 text-white'>
+                         <Link to={'#'} className="no-underline text-white">Өглөөний цайнаас өмнө хүүхэд болгонд өгөх хэрэгтэй 4 зүйлс </Link>
+                    </div>
+                    </div>
                 </div>
-                <div class="row-span-2 col-span-2 ...">
+                <div className="row-span-2 col-span-2 ...">
+                    <div className='relative'>
                     <img src={Blog3} alt="blog1" />
+                    <div className='absolute bottom-10 left-10'>
+                        <Link to={'#'} className="no-underline text-white">ХӨДӨЛГӨӨН <br/> 50 гарсан ч фит биетэй болж болно: Эрчүүдэд өгөх 5 зөвлөмж</Link>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
