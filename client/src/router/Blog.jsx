cafd6bd3d8962d8bc90f818ed1868244fb3c1c66
ganaa0918 <82430690+ganaa0918@users.noreply.github.com>
2022-11-27 23:40:28 +0800

    blog zurag oruulaw

--- client/src/router/Blog.jsx
+++ client/src/router/Blog.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Navbars from '../components/navbar/Navbar'
+import Blog1 from '../image/blog1.png'
+import Blog2 from '../image/blog2.png'
+import Blog3 from '../image/blog3.png'
 
 function Blog() {
   return (
-    <div>Blosg</div>
+    <div className='blog'>
+        <Navbars />
+        <div className='container' style={{paddingTop:"100px"}}>
+            <div class="grid grid-rows-3 grid-flow-col gap-4">
+                <div class="row-span-3 ...">
+                    <img src={Blog1} alt="blog1" />
+                </div>
+                <div class="col-span-2 ...">
+                <img src={Blog2} alt="blog1" />
+                </div>
+                <div class="row-span-2 col-span-2 ...">
+                    <img src={Blog3} alt="blog1" />
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
