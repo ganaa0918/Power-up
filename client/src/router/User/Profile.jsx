79a66bb57fe311aa3e469729c3a2c9251d7e3ade
ganaa0918 <ch.gantulga54@gmail.com>
2022-12-03 16:47:21 +0800

    profile hij duussan

--- client/src/router/User/Profile.jsx
+++ client/src/router/User/Profile.jsx
@@ -9,7 +9,7 @@ function Profile() {
   const togglePasswordVisiblity = () => {
     setPasswordShown(passwordShown ? false : true);
   };
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState: { errors }, } = useForm();
   const onSubmit = data => {
     console.log(data);
   };
@@ -20,40 +20,59 @@ function Profile() {
          <div className='flex'>
             <Sidebar />
                 <div className='container'style={{paddingTop:"100px",}} >
-                <form className='w-96 ms-5 '>
+                <form className='w-96 ms-5 ' method='POST'>
                     <div className="form-group mt-3">
                      <label>Нэр</label>
-                     <input className="form-control" type="text" placeholder="Та нэрээ оруулна уу"/>
+                     <input
+                      className="form-control" 
+                      type="text" 
+                      placeholder="Та нэрээ оруулна уу" 
+                      {...register('Fname',{ required: true })}
+                     
+                      /> {errors.Fname && <p>Нэр оруулна уу</p>}
                      </div>
                      <div className='mt-3 form-group'>
                      <label>Овог</label>
-                     <input className="form-control" type="text" placeholder="Та овог оруулна уу"/>
+                     <input 
+                     className="form-control" 
+                     type="text" 
+                     name='lname'
+                     placeholder="Та овог оруулна уу" 
+                     {...register('lname',{ required: true })}
+                     />
+                     {errors.lname && <p>Овог нэр оруулна уу.</p>}
                      </div>
                      <div className='mt-3 form-group'>
                      <label>Регистр</label>
-                     <input className="form-control" type="text" placeholder="Username"/>
+                     <input 
+                        className="form-control"
+                        type="text" 
+                        placeholder="Username"
+                        {...register('register.',{ required: true })}
+                     />
+                     {errors.register && <p>Регистр оруулна уу</p>}
                      </div> 
                      <div className='mt-3 form-group'>
-                     <label>Гар утас</label>
-                     <input className="form-control" type="number" placeholder="Утасны дугаару"/>
-                     </div>
-                     <div className='mt-3 form-group'>
-                     <label>Password</label>
-                     <input className="form-control" type="" placeholder="Утасны дугаару"/>
+                     <label>Гар утас дугаар</label>
+                     <input 
+                        className="form-control" 
+                        type="number" 
+                        placeholder="Утасны дугаар" 
+                        {...register('number.',{ required: true })} 
+                        required/>
+                     {errors.number && <p>Дугаар оруулна уу</p>}
                      </div>
                      <div className='mt-3 form-group'>
                      <label>Password</label>
                         <input
-                        className="form-control"
-                          placeholder="Password"
+                          className="form-control"
+                          placeholder="password"
                           name="password"
                           type={passwordShown ? "text" : "password"}
-                          {...register('This is required.',{ required: true })}
-                          
-                          // {
-                          //  ...register({ required: "This is required." })
-                          // }
+                          {...register('password',{ required: true })}
                         />
+                       {errors.password && <p>Password оруулна уу</p>}
+                        
                         <div>
                           <FiEye onClick={togglePasswordVisiblity} />
                         </div>
@@ -61,10 +80,20 @@ function Profile() {
                     </div>
                      <div className='mt-3 form-group'>
                      <label>email</label>
-                     <input className="form-control" type="email" placeholder="Email оруулна уу"/>
+                     <input 
+                        className="form-control" 
+                        type="email" 
+                        placeholder="Email оруулна уу" 
+                        {...register('email',{ required: true })}
+                     /> 
+                     {errors.email && <p> email оруулна уу</p>}
                      </div>
                      <div className='d-flex justify-center mt-3'>
-                        <button className='btn'  type="submit" onClick={handleSubmit(onSubmit)}  style={{backgroundColor: "#7A5CFA", color: "white"}}>Засах</button>
+                        <button 
+                        className='btn'  
+                        type="submit" 
+                        onClick={handleSubmit(onSubmit)}  
+                        style={{backgroundColor: "#7A5CFA", color: "white"}}>Засах</button>
                      </div>
                 </form>
                 </div>
