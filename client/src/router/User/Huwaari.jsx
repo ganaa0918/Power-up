8291e38ebda381fbf1b29b8f81e84e6c086d85fc
ganaa0918 <ch.gantulga54@gmail.com>
2022-12-04 18:50:46 +0800

    huwiar

--- client/src/router/User/Huwaari.jsx
+++ client/src/router/User/Huwaari.jsx
@@ -3,19 +3,58 @@ import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import Configobj from './chartdata';
 import Sidebar from '../../components/sidebar/Sidebar';
+import DataTable from 'react-data-table-component';
 function Huwaari() {
+  const columns = [
+    {
+        name: 'Name',
+        selector: row => row.name,
+    },
+    {
+        name: 'Day',
+        selector: row => row.day,
+    },
+    {
+        name: 'Status',
+        selector: row => row.status,
+    },
+];
+
+const data = [
+    {
+        id: 1,
+        name: 'Beetlejuice',
+        day: 'Даваа',
+        status: 'Active',
+        
+
+    },
+]
   return (
-   <div>
      <div className='flex'>
      <Sidebar />
      <div className='container'style={{paddingTop:"100px",}}>
-    <HighchartsReact
-    highcharts={Highcharts}
-    options={Configobj}
-  />
+        <div className='mb-2'>
+      <HighchartsReact
+      highcharts={Highcharts}
+      options={Configobj}
+        />
       </div>
+      <div> <DataTable
+                title="Захиалгын түүх"
+                columns={columns}
+                data={data}
+                pagination
+                paginationPerPage={5}
+                paginationRowsPerPageOptions={[5,10]}
+                theme='light'
+                noDataComponent={'Жагсаалт байхгүй байна'}
+                paginationComponentOptions={
+                    {rowsPerPageText: 'Хуудасны тоо:'}
+                }
+                /></div>
     </div>
-   </div>
+    </div>
   )
 }
 
